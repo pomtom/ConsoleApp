b5108dc6483eaf777255aa61f0de7f2b4c89d30e
Pramod Lawate <pramod.lawate@btl.com>
2023-07-22 18:10:26 +0100

    #Added Order by Operator Demo

---
+++ OrderOperator/EmployeeClone.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderOperator
+{
+    internal class EmployeeClone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ OrderOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿using Database;
+
+namespace OrderOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new TryDbContext();
+
+
+            Console.WriteLine($"Before order by ascending");
+            var employees = context.Employees;
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            var emps = employees;
+            Console.WriteLine($"After Order by Ascending");
+            foreach (var employee in emps.OrderBy(a => a.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.WriteLine($"After Order by Descending");
+            foreach (var employee in emps.OrderByDescending(a => a.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+}
